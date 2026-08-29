7666f06ba76fe066033f520a374c3e4266e36581
rojasflorsele-899 <rojasflorsele@gmail.com>
2026-08-28 20:33:47 -0400

    Delete homepage directory

--- homepage/scrpt.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const mainNav = document.querySelector(".main-nav");
-
-menuToggle.addEventListener("click", function () {
-    mainNav.classList.toggle("active");
-});
